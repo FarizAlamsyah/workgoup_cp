2f4649950433ad2c632ca8cbf64b379d8bb6c985
alvincf10 <alvinwijaya446@gmail.com>
2019-09-01 19:19:19 +0700

    nih front endnya tinggal css

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,14 @@
 {{-- untuk menghubungkan template index --}}
 @extends('index')
 {{-- ini judul halaman --}}
-@section('halaman','Halaman About')
+@section('halaman','Tentang Kami')
 {{-- ini isi kontent --}}
 @section('kontent')
-Ini Adalah Halaman About
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam, quo eius velit nesciunt
+     corrupti  voluptate libero, quaerat asperiores  aliquid non architecto accusantium, 
+     aliquam nam rem quos? Quas laudantium iure vitae.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate beatae consectetur nisi, quaerat accusantium hic modi at alias 
+    dolore dignissimos cum corrupti ipsam, mollitia aperiam! 
+    Expedita quidem quod praesentium aspernatur.</p>
 
 @endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,10 +1,54 @@
 {{-- untuk menghubungkan template index --}}
 @extends('index')
 {{-- ini judul halaman --}}
-@section('halaman','Halaman Contact')
+@section('halaman',)
 {{-- ini isi kontent --}}
 @section('kontent')
 
-<p>Ini Adalah Halaman Contact</p>
+<section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Hubungi Kami</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Nama *" id="name" required data-validation-required-message="Please enter your name.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" placeholder="Email *" id="email" required data-validation-required-message="Please enter your email address.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input type="tel" class="form-control" placeholder="No Telp *" id="phone" required data-validation-required-message="Please enter your phone number.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <textarea class="form-control" placeholder="Pesan *" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="col-lg-12 text-center">
+                                <div id="success"></div>
+                                <button type="submit" class="btn btn-xl">Kirim Pesan</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 
 @endsection
+
+
